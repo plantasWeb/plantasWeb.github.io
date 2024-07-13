3422f0134c6f14698070f8a0db247ee9d0d6f255
plantasWeb <github2004z@gmail.com>
2024-07-12 22:03:51 -0500

    innitial commit

---
+++ JS/validarUsuario.js
@@ -0,0 +1,19 @@
+/*document.addEventListener('DOMContentLoaded', function () {
+
+    const password = prompt("Por favor, ingresa la contraseña para acceder a la página:");
+
+    // Definir la contraseña correcta
+    const correctPassword = "12345";
+
+    // Validar la contraseña
+    if (password !== correctPassword) {
+        alert("Contraseña incorrecta. No tienes acceso a la página.");
+        document.body.innerHTML = "<h1>Acceso denegado</h1>";
+        return;
+    }
+
+    
+})*/
+
+
+
