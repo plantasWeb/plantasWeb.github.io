caeeb3877fa2da1963302a88de241fab722c1b3e
plantasWeb <github2004z@gmail.com>
2024-07-13 23:02:46 -0500

    hello

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+/*const contenedor = document.querySelector('flex-container')
+
+function crearProducto(nombre,tipo,precio){
+    nombre = `<h3 style='color:white'>${nombre}</h3>`;
+    tipo = `<h2 style='color:white'>${tipo}</h2>`;
+    precio = `<p style='color:white'>${precio}</p>`
+    return [nombre, tipo, precio]
+}
+
+const producto = crearProducto('nja','dqwdw','ewrew');
+
+document.write(producto[0],producto[1],producto[2]);
+
+contenedor.innerHTML = producto[0] + producto[1] + producto[2];*/
